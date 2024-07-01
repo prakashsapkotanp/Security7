a74c2c9a462f5f9770464de90eeddfb5b5593a3a
stezan1 <shahuchandan537@gmail.com>
2024-07-01 20:22:05 +0545

    requests showing by stezan

--- springjwtsecurity/src/main/java/com/spring3/oauth/jwt/controllers/RequestController.java
+++ springjwtsecurity/src/main/java/com/spring3/oauth/jwt/controllers/RequestController.java
@@ -2,26 +2,37 @@ package com.spring3.oauth.jwt.controllers;
 
 import com.spring3.oauth.jwt.models.Request;
 import com.spring3.oauth.jwt.models.RequesterInfo;
+import com.spring3.oauth.jwt.repositories.RequestRepository;
 import com.spring3.oauth.jwt.services.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/requests")
 public class RequestController {
 
-    @Autowired
-    private RequestService requestService;
+//    @Autowired
+//    private RequestService requestService;
+//
+//    @PostMapping
+//    public ResponseEntity<String> createRequest(@RequestBody RequesterInfo requester) {
+//        requestService.createRequest(requester);
+//        return ResponseEntity.ok("Request sent to nearby donors");
+//    }
+//    @PutMapping("/{id}")
+//    public ResponseEntity<String> updateRequest(@PathVariable Long id, @RequestBody Request updatedRequest) {
+//        requestService.updateRequest(id, updatedRequest);
+//        return ResponseEntity.ok("Request updated successfully");
+//    }
+
+@Autowired
+private RequestRepository requestRepository;
 
     @PostMapping
-    public ResponseEntity<String> createRequest(@RequestBody RequesterInfo requester) {
-        requestService.createRequest(requester);
-        return ResponseEntity.ok("Request sent to nearby donors");
-    }
-    @PutMapping("/{id}")
-    public ResponseEntity<String> updateRequest(@PathVariable Long id, @RequestBody Request updatedRequest) {
-        requestService.updateRequest(id, updatedRequest);
-        return ResponseEntity.ok("Request updated successfully");
+    public List<Request> getAllRequests() {
+        return requestRepository.findAll();
     }
 }
